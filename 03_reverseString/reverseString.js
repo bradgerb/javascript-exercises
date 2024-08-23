aaddf382c4ad0a0f77e87d36e315bbdec2f07f6b
bradgerb <77023448+bradgerb@users.noreply.github.com>
2024-08-23 17:40:41 -0400

    First attempt

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,13 @@
-const reverseString = function() {
+const reverseString = function(string) {
+
+    let length = string.length;
+    let answer = "";
+
+    for (i = length-1; i = 0; i--){
+        answer = answer + string.charAt(i);
+    }
+
+    return answer;
 
 };
 
