611a19bc89573bd0ab8000233409c06bb711c92e
bradgerb <77023448+bradgerb@users.noreply.github.com>
2024-08-23 16:52:21 -0400

    Write code to pass test 1

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    
+    let answer = "";
 
+    for (i = 0; i < num; i++){
+        answer = answer + string;
+    }
+
+    return answer;
 };
 
 // Do not edit below this line
