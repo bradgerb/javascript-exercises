db372d55a301f90e35a15059d3138f8e89e4705b
bradgerb <77023448+bradgerb@users.noreply.github.com>
2024-08-23 23:13:33 -0400

    Pass test 1

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,12 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+
+    let answer = 0;
+
+    for (i = start; i <= end; i++){
+        answer = answer + i;
+    }
+
+    return answer
 
 };
 
