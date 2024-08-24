5d606392fdb8a02074aab1e21d6a97e39237d365
bradgerb <77023448+bradgerb@users.noreply.github.com>
2024-08-23 22:48:10 -0400

    Write splice to remove value

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(arrayIn, remove) {
+
+    let length = arrayIn.length;
+    let answer = [];
+
+    for (i = 0; i < length; i++){
+        if (arrayIn.at(i) === remove){
+            deletedValue = arrayIn.splice(i, 1);
+        }
+    }
+
+    return arrayIn
+
 };
 
 // Do not edit below this line
