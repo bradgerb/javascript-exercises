5c8f3725d87eaf5a240d8ff860a22b41647718b7
bradgerb <77023448+bradgerb@users.noreply.github.com>
2024-09-06 14:54:55 -0400

    Write sum function to pass tests 5-8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -6,8 +6,8 @@ const subtract = function(a, b) {
 	return a-b
 };
 
-const sum = function() {
-	
+const sum = function(input) {
+  return input.reduce((sum, current) => sum + current, 0)
 };
 
 const multiply = function() {
