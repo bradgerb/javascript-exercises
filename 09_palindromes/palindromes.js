806b05d29349abff044d10a461089ef0f1a9339f
bradgerb <77023448+bradgerb@users.noreply.github.com>
2024-09-06 15:55:02 -0400

    Write function to work without punctuation

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,17 @@
-const palindromes = function () {
+const palindromes = function (text) {
+    
+    let length = text.length;
+    let answer = "";
+
+    for (i = length-1; i >= 0; i--){
+        answer = answer + text.charAt(i);
+    }
+
+    if (answer === text){
+        return true
+    } else{
+        return false
+    }
 
 };
 
