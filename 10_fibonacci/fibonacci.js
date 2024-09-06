9f5c9b76422934ff5ecaf72f5ada9e47bbd62a01
bradgerb <77023448+bradgerb@users.noreply.github.com>
2024-09-06 16:54:28 -0400

    Pass all tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let answer = 0;
+    let previousOne = 1;
+    let previousTwo = 0;
 
+    parseInt(num, 10);
+
+    if (num === 0){
+        return 0
+    }else if (num == 1){
+        return 1
+    } else if(num < 0){
+        return "OOPS"
+    } else{
+        for (i = 0; i < num-1; i++){
+            answer = previousOne + previousTwo;
+            previousTwo = previousOne;
+            previousOne = answer;
+        };
+    };
+    return answer
 };
 
 // Do not edit below this line
