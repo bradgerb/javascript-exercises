7294eed1d0d4060f20f603c733df021e2936a1a1
bradgerb <77023448+bradgerb@users.noreply.github.com>
2024-09-09 16:04:29 -0400

    Pass all tests

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,25 @@
-const findTheOldest = function() {
+const date = new Date();
+let year = date.getFullYear();
+
+const findTheOldest = function(listOfPeople) {
+    
+    const sortedAges = listOfPeople.sort(function(a, b){
+        if (!a.yearOfDeath){
+            a.yearOfDeath = year;
+        };
+        if (!b.yearOfDeath){
+            b.yearOfDeath = year;
+        }
+        const ageA = a.yearOfDeath-a.yearOfBirth;
+        const ageB = b.yearOfDeath-b.yearOfBirth;
+        if (ageA > ageB){
+            return -1
+        }else{
+            return 1
+        };
+    });
+
+    return (sortedAges[0])
 
 };
 
