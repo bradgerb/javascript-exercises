d2a0161b1c6fda89a18a2bcde1fa53cd8a9a7c95
bradgerb <77023448+bradgerb@users.noreply.github.com>
2024-09-10 14:02:09 -0400

    Create whitelist and filter

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -2,6 +2,16 @@ const palindromes = function (text) {
     
     let length = text.length;
     let answer = "";
+    let whitelist = "abcdefghijklmnopqrstuvwxyz0123456789"
+
+    text = text.toLowerCase()
+
+    text = text.split("");
+    text = text.filter(cleanUp);
+        function cleanUp(clean){
+            return whitelist.includes(clean);
+        }
+    text = text.join("");
 
     for (i = length-1; i >= 0; i--){
         answer = answer + text.charAt(i);
